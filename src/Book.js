7786a62456557b1d6b40a4ab4b8661fb489fd31e
Gabriel C Moraru <gabrielcmoraru@gmail.com>
2018-07-24 00:30:55 +0100

    feat: implemented dropdown menu to book; todo: add state func

--- src/Book.js
+++ src/Book.js
@@ -2,11 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import Dropdown from 'react-dropdown';
 import Overdrive from 'react-overdrive';
+
+const options = [
+  { value: 'one', label: 'Currently Reading' },
+  { value: 'two', label: 'Want to Read' },
+  { value: 'three', label: 'Read' },
+];
+const defaultOption = options[3];
 
 const Book = ({ book }) => (
   <div>
     <Cover src={book.imageLinks.thumbnail} alt={book.title} />
+    <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Shelfing Options " />
   </div>
 );
 
@@ -20,6 +29,6 @@ Book.propTypes = {
 
 export const Cover = styled.img`
   box-shadow: 0 0 35px;
-  height: 205px;
+  height: 225px;
   width: 165px;
 `;
